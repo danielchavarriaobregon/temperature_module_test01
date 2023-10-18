024c0f9c36746759b39121928d6964da74e854f5
danielchavarriaobregon <147556908+danielchavarriaobregon@users.noreply.github.com>
2023-10-18 14:03:07 -0600

    Add files via upload

--- temp_test/src/main.cpp
+++ temp_test/src/main.cpp
@@ -1,20 +1,71 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
-//high led red with delay//****
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <EEPROM.h>
 
+// LED RGB
 int ledr = 4;
+define ledr, OUTPUT;
 
-void setup()
-{
+// LCD DISPLAY
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+// SENSOR
+const int oneWireBus = 12;
+OneWire oneWire(oneWireBus);
+DallasTemperature sensors(&oneWire);
+
+// Constants definition EEPROM
+#define EEPROM_SIZE 4096
+
+int timeAddress = 100;
+float lastTemperature = 0;
+
+void readData();
+
+void setup() {
+  Wire.begin(8, 9);
   pinMode(ledr, OUTPUT);
-  //para prender el led se necesita la funcion digitalWrite(pin, value);
-  while (true)
-  {
-    digitalWrite(ledr, HIGH);
-    delay(2000);
-    digitalWrite(ledr, LOW);
-    delay(2000);
-  }
+  lcd.init();
+  lcd.backlight();
+  sensors.begin();
+  EEPROM.begin(EEPROM_SIZE);
+  lastTemperature = EEPROM.readFloat(timeAddress);
+  readData();
 }
 
-void loop(){}
+void loop() {
+  readData();
+  delay(1000); // Delay for 10 seconds before reading data again
+}
+
+void readData() {
+  sensors.requestTemperatures();
+  float currentTemperature = sensors.getTempCByIndex(0);
+  EEPROM.writeFloat(timeAddress, currentTemperature);
+  EEPROM.commit();
+
+  float difference = currentTemperature - lastTemperature;
+
+  lcd.setCursor(0, 2);
+  lcd.print("Difference: ");
+  lcd.print(difference);
+
+  if (difference >= 2 || difference <= -2) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("ALERTA");
+    digitalWrite(ledr, HIGH);
+  }
+  else
+  {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("all good");
+    digitalWrite(ledr, LOW);
+  }
+
+  lastTemperature = currentTemperature;
+}
+ 
